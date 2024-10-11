9688f998f300d8f8b6c22905cda79e7b2bf13ea3
Anuj Verma <anujbefog@gmail.com>
2024-10-11 22:39:02 +0530

    ExitAndEnterAnimation & Keyframes Completed

--- FramerMotion/animate_transition/src/App.jsx
+++ FramerMotion/animate_transition/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-
 import './App.css'
-import ProgressBar from './framer-components/ProgressBar'
+import KeyFrames from './framer-components/KeyFrames'
+// import ExitAndEnterAnimation from './framer-components/ExitAndEnterAnimation'
+// import ProgressBar from './framer-components/ProgressBar'
 
 const App = () => {
   return (
     <>
-      <div className='p-10 flex flex-col'>
-        <h1 className='text-center text-4xl font-semibold'>
+      <div className='p-10 flex h-screen flex-col bg-black'>
+        <h1 className='text-center text-white text-4xl font-semibold'>
           Framer motion components
         </h1>
-        <ProgressBar />
+        {/* <ProgressBar /> */}
+        {/* <ExitAndEnterAnimation /> */}
+        <KeyFrames />
       </div>
     </>
   )
---
+++ FramerMotion/animate_transition/src/framer-components/ExitAndEnterAnimation.jsx
@@ -0,0 +1,32 @@
+import { AnimatePresence, motion } from "framer-motion"
+import { useEffect, useState } from "react"
+
+const ExitAndEnterAnimation = () => {
+    const [removeInnerCircle, setRemoveInnerCircle] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setRemoveInnerCircle(true)
+        }, 5000);
+    })
+
+    return (
+        <div>
+            <div className="bg-white h-32 w-32 rounded-full mx-auto mt-10">
+                <AnimatePresence>
+                    {removeInnerCircle === false && (
+                        <motion.div
+                            initial={{ opacity: 0, y: 0 }}
+                            animate={{ opacity: 1, y: 40 }}
+                            transition={{ duration: 5 }}
+                            exit={{ opacity: 0, y: 1000 }}
+                            className="bg-black h-24 w-24 rounded-full">
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
+        </div>
+    )
+}
+
+export default ExitAndEnterAnimation
